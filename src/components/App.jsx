b853d65a62299db01ecf2cccaa99c011e7630004
Piron-Johny <piron.johny@gmail.comm>
2022-02-20 23:14:13 +0200

    Готово. Перенес запрос в App. Переписал пропсы.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,69 @@
 import { Component } from 'react';
 import SearchBar from './SearchBar';
 import ImageGallery from './ImageGallery';
+import { fetchMoviesWithQuery } from 'services/api';
 
 class App extends Component {
   state = {
     searchValue: '',
+    isPending: false,
+    images: [],
+    page: 1,
+    loader: false,
   };
+
+  async componentDidUpdate(prevProps, prevState) {
+    const { isPending, searchValue, page } = this.state;
+
+    if (isPending) {
+      try {
+        const fetchImages = await fetchMoviesWithQuery(searchValue, page);
+        this.setState(({ images }) => ({
+          images: page > 1 ? [...images, ...fetchImages] : fetchImages,
+          loader: false,
+          isPending: false,
+        }));
+
+        if (page > 1) {
+          window.scrollTo({
+            top: document.documentElement.scrollHeight,
+            behavior: 'smooth',
+          });
+        }
+      } catch (error) {
+        this.setState({ error: error.response.error });
+      }
+    }
+  }
 
   handleFormSubmit = searchValue => {
-    this.setState({ searchValue });
+    this.setState({ searchValue, isPending: true, page: 1, loader: true });
+    window.scrollTo({
+      top: '',
+      behavior: 'smooth',
+    });
   };
 
+  onLoadMore = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+      isPending: true,
+      loader: true,
+    }));
+  };
+
+  // ======================== RENDER ==================================
+
   render() {
-    const { searchValue } = this.state;
+    const { loader, images } = this.state;
     return (
       <>
         <SearchBar onSubmit={this.handleFormSubmit} />
-        <ImageGallery searchValue={searchValue} />
+        <ImageGallery
+          images={images}
+          onLoadMore={this.onLoadMore}
+          loader={loader}
+        />
       </>
     );
   }
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,6 +1,4 @@
 import { Component } from 'react';
-import { fetchMoviesWithQuery } from 'services/api';
-import { Paragraph } from './ImageGallery.styled';
 import Modal from '../Modal';
 import ImageList from '../ImageList';
 import Loader from '../Loader';
@@ -8,51 +6,8 @@ import Button from '../Button';
 
 class ImageGallery extends Component {
   state = {
-    images: [],
-    page: 1,
-    loading: false,
-    searchValue: '',
-    error: null,
-    status: 'idle',
     largeImg: '',
     isModal: false,
-  };
-
-  async componentDidUpdate(prevProps, prevState) {
-    const nextSearchValue = this.props.searchValue;
-
-    if (
-      prevProps.searchValue !== nextSearchValue ||
-      prevState.page !== this.state.page // ИЗ-ЗА ТОГО, ЧТО ТУТ УСЛОВИЕ ОБНОВЛЕНИЯ PAGE. НЕ ЗНАЮ КАК СКИНУТЬ ПРИ САБМИТЕ
-    ) {
-      if (prevProps.searchValue !== nextSearchValue) {
-        this.setState({ images: [], page: 1 }); // ПРОБЛЕМА В ОБНУДЕНИИ PAGE. ПРИ ДРУГОМ ЗАПРОСЕ ОТОБРАЖАЕТСЯ ПЕРВАЯ И ТЕКУЩАЯЯ СТРАНИЦА
-      }
-
-      this.setState({ searchValue: nextSearchValue, loading: true });
-      const { page } = this.state;
-
-      try {
-        const fetchImages = await fetchMoviesWithQuery(nextSearchValue, page);
-        this.setState(({ images }) => ({
-          images: page > 1 ? [...images, ...fetchImages] : fetchImages,
-          loading: false,
-        }));
-
-        if (this.state.page > 1) {
-          window.scrollTo({
-            top: document.documentElement.scrollHeight,
-            behavior: 'smooth',
-          });
-        }
-      } catch (error) {
-        this.setState({ error: error.response.error });
-      }
-    }
-  }
-
-  onLoadMore = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
   };
 
   onOpenModal = e => {
@@ -67,7 +22,8 @@ class ImageGallery extends Component {
   // --------------------------------render------------------------
 
   render() {
-    const { images, loading, isModal, largeImg } = this.state;
+    const { isModal, largeImg } = this.state;
+    const { images, onLoadMore, loader } = this.props;
 
     if (!!images.length) {
       return (
@@ -80,7 +36,7 @@ class ImageGallery extends Component {
 
           <ImageList images={images} onOpenModal={this.onOpenModal} />
 
-          {loading ? <Loader /> : <Button onLoadMore={this.onLoadMore} />}
+          {loader ? <Loader /> : <Button onLoadMore={onLoadMore} />}
         </>
       );
     }
